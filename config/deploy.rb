8935ba9481ae197e1833b079c1c17f73203ca162
= <davidbritton.ballard@gmail.com>
2010-02-20 16:17:38 -0600

    overriding start, stop, restart for now

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,10 +14,9 @@ server "sweetboyalliance.com", :app, :web, :db, :primary => true
 # if you're still using the script/reapear helper you will need
 # these http://github.com/rails/irs_process_scripts
 
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app do ; end
+end
+
