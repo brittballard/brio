074b336bd5dc078f6312d0e17196efaeba9db9e1
= <davidbritton.ballard@gmail.com>
2010-02-20 15:40:57 -0600

    Added process folder to scripts folder for cap deployment.

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,14 +2,13 @@ set :application, "brio"
 set :repository,  "git@github.com:brittballard/brio.git"
 set :deploy_to,    "/var/rails/#{fetch(:application)}"
 set :keep_releases, false
+set :use_sudo, false
 
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 set :user, "deploy"
 
-role :web, "sweetboyalliance.com"                          # Your HTTP server, Apache/etc
-role :app, "sweetboyalliance.com"                          # This may be the same as your `Web` server
-role :db,  "sweetboyalliance.com", :primary => true # This is where Rails migrations will run
+server "sweetboyalliance.com", :app, :web, :db, :primary => true
 
 # If you are using Passenger mod_rails uncomment this:
 # if you're still using the script/reapear helper you will need
