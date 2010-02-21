352a7ba208449939efdebc61441166e310327500
= <davidbritton.ballard@gmail.com>
2010-02-20 23:32:11 -0600

    Added skeleton code for events (don't worry jshan I didn't steal your thunder still plenty to do)

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100221044224) do
+ActiveRecord::Schema.define(:version => 20100221052916) do
+
+  create_table "events", :force => true do |t|
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                              :null => false
