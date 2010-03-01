e7dc53d7f70f7552b21bbe6ae501cb625924608e
Geoffrey <foeggy@gschiff.(none)>
2010-02-28 19:55:30 -0600

    lock down some of the event happenings

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,9 @@
 class EventsController < ApplicationController
-
+  before_filter :require_user, :only => [:search_my, :create, :update, :edit]
+    
   def index
     @events = Event.all
-    
+
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @events }
@@ -16,46 +17,46 @@ class EventsController < ApplicationController
   def new
     @event = Event.new
   end
-  
+
   def search_open
     # pull the search items out
     date = params[:date]
     name = params[:name].present? ? '%'+params[:name]+'%' : '%'
     location = params[:location]
-      
+
     # pass that shit into the search homie and give it to the search list
-    # WOH WOH WOH:  use the date to search that it is between the start and end date 
+    # WOH WOH WOH:  use the date to search that it is between the start and end date
     @events = Event.registration_open.all(:conditions => ["title LIKE ?", name])
-    
+
     respond_to do |format|
       format.html
     end
   end
-  
+
   def search_my
     # pull the search items out
     date = params[:date]
     name = params[:name].present? ? '%'+params[:name]+'%' : '%'
     location = params[:location]
-      
+
     @events = Event.all(:joins => :registrants, :conditions => ["user_id = ? AND title LIKE ?", current_user.id, name])
-      
+
     respond_to do |format|
       format.html
     end
   end
-  
+
   def search_results
     # pull the search items out
     date = params[:date]
     name = params[:name].present? ? '%'+params[:name]+'%' : '%'
     location = params[:location]
-      
+
     # pass that shit into the search homie and give it to the search list
-    # WOH WOH WOH:  use the date to search that it is between the start and end date 
+    # WOH WOH WOH:  use the date to search that it is between the start and end date
     @events = Event.complete.all(:conditions => ["title LIKE ?", name])
   end
-  
+
   def create
     @event = Event.new(params[:event])
     if @event.save
@@ -70,25 +71,16 @@ class EventsController < ApplicationController
   def edit
     @event = Event.find(params[:id])
   end
-  
+
   def update
     @event = Event.find(params[:id])
-    
+
     if @event.update_attributes(params[:event])
       flash[:notice] = "Event updated succesfully."
       redirect_to @event
     else
       flash[:error] = "Error updating event."
       render :action => 'edit'
-    end
-  end
-  
-  def search
-    # pair this down to use the filter dawg
-    @events = Event.all
-    
-    respond_to do |format|
-      format.html
     end
   end
 
