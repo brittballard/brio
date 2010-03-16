7ddb2ee76d37e2aff7106abc6f45487860df298c
Britton Ballard <davidbritton.ballard@gmail.com>
2010-03-15 23:16:01 -0500

    First steps towards integrating google maps.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,47 +1,47 @@
 // utility methods
 
 jQuery.ajaxSetup({
-	'beforeSend': function(xhr) {
-	        xhr.setRequestHeader("Accept", "text/javascript")
-	    }
+  'beforeSend': function(xhr) {
+          xhr.setRequestHeader("Accept", "text/javascript")
+      }
 })
 
 jQuery.extend({
-	delete_post: function( url, data, callback, type ) {
-		// shift arguments if data argument was omited
-		if ( jQuery.isFunction( data ) ) {
-			type = type || callback;
-			callback = data;
-			data = {};
-		}
+  delete_post: function( url, data, callback, type ) {
+    // shift arguments if data argument was omited
+    if ( jQuery.isFunction( data ) ) {
+      type = type || callback;
+      callback = data;
+      data = {};
+    }
 
-		return jQuery.ajax({
-			type: "DELETE",
-			url: url,
-			data: data,
-			success: callback,
-			dataType: type
-		});
-	}
+    return jQuery.ajax({
+      type: "DELETE",
+      url: url,
+      data: data,
+      success: callback,
+      dataType: type
+    });
+  }
 })
 
 jQuery.extend({
-	put_post: function( url, data, callback, type ) {
-		// shift arguments if data argument was omited
-		if ( jQuery.isFunction( data ) ) {
-			type = type || callback;
-			callback = data;
-			data = {};
-		}
+  put_post: function( url, data, callback, type ) {
+    // shift arguments if data argument was omited
+    if ( jQuery.isFunction( data ) ) {
+      type = type || callback;
+      callback = data;
+      data = {};
+    }
 
-		return jQuery.ajax({
-			type: "PUT",
-			url: url,
-			data: data,
-			success: callback,
-			dataType: type
-		});
-	}
+    return jQuery.ajax({
+      type: "PUT",
+      url: url,
+      data: data,
+      success: callback,
+      dataType: type
+    });
+  }
 })
 
 var brio = {}
@@ -50,4 +50,14 @@ brio.register_date_picker = function(ids){
   for(var index in ids){
     $('#' + ids[index]).datepicker();
   }
+}
+
+brio.initialize_map = function(lat, lng, canvas_id){
+  var latlng = new google.maps.LatLng(lat, lng);
+  var myOptions = {
+    zoom: 8,
+    center: latlng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById(canvas_id), myOptions);
 }
