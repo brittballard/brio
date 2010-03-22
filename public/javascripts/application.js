7f69a3202fe8874ed0aaa926deafe7d6bbb6306a
Britton Ballard <davidbritton.ballard@gmail.com>
2010-03-21 23:58:53 -0500

    Added support for click to view street view. Up next setting gio points and moving along those bad boys.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -46,6 +46,9 @@ jQuery.extend({
 // namespaces
 var brio = {}
 brio.utility = {}
+brio.utility.map = {}
+brio.utility.pano = {}
+
 brio.jQueryInit = {}
 
 brio.utility.register_date_picker = function(ids){
@@ -54,23 +57,33 @@ brio.utility.register_date_picker = function(ids){
   }
 }
 
-brio.utility.initialize_map = function(lat, lng, canvas_id){
-  var latlng = new google.maps.LatLng(lat, lng);
-  var myOptions = {
-    zoom: 8,
-    center: latlng
-  };
-  
-  var map = new Map(document.getElementById(canvas_id), myOptions);
+brio.utility.map.initialize_map = function(lat, lng, zoom, map_canvas_id, pano_canvas_id, overlays){
+  if (GBrowserIsCompatible()) {
+    var map = new GMap2(document.getElementById(map_canvas_id));
+    map.setCenter(new GLatLng(lat, lng), zoom);
+    map.setUIToDefault();
+
+    if(overlays != null && overlays.length > 0){
+      for(var i = 0; i < overlays.length; i++){
+        map.addOverlay(overlays[i]);
+      }
+    }
+    
+    if(pano_canvas_id != null){
+      var pano = brio.utility.pano.initialize_panorama(pano_canvas_id);
+      
+      GEvent.addListener(map,"click", function(overlay, latlng) {
+        pano.setLocationAndPOV(latlng);
+      });
+    }
+  }
 }
 
-brio.utility.initialize_panorama = function(lat, lng, canvas_id){
-  var streetView = new google.maps.LatLng(lat, lng);
-  panoramaOptions = { latlng:streetView };
-  var myPano = new GStreetviewPanorama(document.getElementById(canvas_id), panoramaOptions);
+brio.utility.pano.initialize_panorama = function(canvas_id){
+  return new GStreetviewPanorama(document.getElementById(canvas_id));
 }
 
-brio.utility.animate = function(lat, lng, canvas_id){
+brio.utility.pano.animate = function(lat, lng, canvas_id){
   var myPano = new GStreetviewPanorama(document.getElementById(canvas_id));
   var location = new GLatLng(lat, lng);
   myPano.setLocationAndPOV(location, myPano.getPOV());
