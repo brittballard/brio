2795d68eb9bcaf9259c151412b6b88be3265b100
Britton Ballard <davidbritton.ballard@gmail.com>
2010-03-26 00:38:19 -0500

    Added a simple functional test. I'll add more exahstive tests tomorrow.

--- test/functional/events_controller_test.rb
+++ test/functional/events_controller_test.rb
@@ -1,8 +1,14 @@
 require 'test_helper'
 
 class EventsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  context("on get to :index show events") do
+    setup do
+      get(:index)
+    end
+    
+    should_assign_to(:events)
+    should_respond_with(:success)
+    should_render_template(:index)
+    should_not_set_the_flash
   end
 end
