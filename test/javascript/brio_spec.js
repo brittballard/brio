f8a4e806b1b7a3eb8c68a6fbfff34a0ac57042e8
Britton Ballard <davidbritton.ballard@gmail.com>
2010-03-25 23:57:54 -0500

    Building out events tests to give sweet boy a nice example.

--- test/javascript/brio_spec.js
+++ test/javascript/brio_spec.js
@@ -1,20 +1,31 @@
 require("spec_helper.js");
 require("../../public/javascripts/brio.js");
 
-
 Screw.Unit(function() {
   describe("brio", function() {
     after(function(){ teardownFixtures() });
     
-    describe("register_date_picker", function(){
-      it("takes a list of div ids and runs the datepicker jQuery ui method on them", function(){
-        fixture($('<div id="brittonstest" />'));
+    describe("utility", function(){
 
-        var test_datepicker = $('#brittonstest');
-        date_picker_mock = mock(test_datepicker);
-        date_picker_mock.should_receive('datepicker').exactly('once');
+      describe("register_date_picker", function(){
+        it("takes a list of div ids and runs the datepicker jQuery ui method on them", function(){
+          fixture($('<div id="brittonstest" />'));
+
+          var test_datepicker = $('#brittonstest');
+          date_picker_mock = mock(test_datepicker);
+          date_picker_mock.should_receive('datepicker').exactly('once');
         
-        brio.utility.register_date_picker([test_datepicker]);
+          brio.utility.register_date_picker([test_datepicker]);
+        });
+        
+        describe("map", function(){
+
+          describe("initialize_map", function(){
+            it("takes a lat, lng, div id for the map and pano, and an array of overlays and creates a google map", function(){
+
+            });
+          });
+        });
       });
     });
   });
--- test/unit/event_test.rb
+++ test/unit/event_test.rb
@@ -1,15 +1,40 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  context "creating events" do
-    should "create a datetime value when a start_date, start_hour, and start_minute are provided" do
-      flexmock(Event)
-      event = Factory.build(:event)
-      event.start_date = "03/11/1980"
-      event.start_hour = "5"
-      event.start_minute = "05"
-      event.set_date_time('start')
-      assert_equal(DateTime.parse("03/11/1980 5:05"), event.start_date_time)
+  subject { Factory.build(:event) }
+
+  should_have_one(:map)
+  should_have_and_belong_to_many(:registrants)
+  should_validate_presence_of(:title, :description, :start_date_time, :end_date_time, :address_line_1, :city, :state, :postal_code, :time_zone)
+  
+  
+  context("creating events") do
+    setup do
+      @event = Factory.build(:event)
+    end
+    
+    should("create a datetime value when a start_date, start_hour, and start_minute are provided") do
+      @event.start_date   = "03/11/1980"
+      @event.start_hour   = "5"
+      @event.start_minute = "05"
+      @event.set_date_time('start')
+      assert_equal(DateTime.parse("03/11/1980 5:05"), @event.start_date_time)
+    end
+    
+    should("fail if the start date is after end date") do
+      @event.start_date_time  = Time.now
+      @event.end_date_time    = 1.day.ago
+      assert(!@event.save)
+      assert_equal(1, @event.errors.length)
+      assert(@event.errors[:start_date_time].present?)
+    end
+    
+    should("fail if the registration start date is after the registration end date") do
+      @event.registration_start_date_time  = Time.now
+      @event.registration_end_date_time    = 1.day.ago
+      assert(!@event.save)
+      assert_equal(1, @event.errors.length)
+      assert(@event.errors[:registration_start_date_time].present?)
     end
   end
 end
