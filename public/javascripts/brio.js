a6dbd3ea0e7058eeb4a267ebd4de6cc7484bce85
Britton Ballard <davidbritton.ballard@gmail.com>
2010-03-29 00:10:44 -0500

    Refactored javascript namespace and added tests for all existing methods. I'm having an issue with Smoke not working like i expect. Currently if a mock expect isn't satisfied the tests just stop running rather than reporting where the failure occurred.

--- public/javascripts/brio.js
+++ public/javascripts/brio.js
@@ -1,4 +1,4 @@
-var brio = (function(){
+var brio = (function($){
   return {
     utility: {
       register_date_picker : function(date_pickers){
@@ -7,38 +7,26 @@ var brio = (function(){
         }
       },
       
-      set_data : function(id, data_defs){
-        var dom_object = document.getElementById(id);
-      
+      set_data : function(element, data_defs){
         for(var i = 0; i < data_defs.length; i++){
-          $(dom_object).data(data_defs[i][0], data_defs[i][1]);
+          element.data(data_defs[i][0], data_defs[i][1]);
         }
       },
 
       map: {
-        initialize_map : function(lat, lng, zoom, map_canvas_id, pano_canvas_id, overlays){
-          if (GBrowserIsCompatible()) {
-            var map_div = document.getElementById(map_canvas_id);
-            var map = new GMap2(map_div);
-            map.setCenter(new GLatLng(lat, lng), zoom);
+        initialize_map : function(map, lat_lng, zoom){
+            map.setCenter(lat_lng, zoom);
             map.setUIToDefault();
 
-            if(overlays != null && overlays.length > 0){
-              for(var i = 0; i < overlays.length; i++){
-                map.addOverlay(overlays[i]);
-              }
-            }
-
-            if(pano_canvas_id != null){
-              var pano = brio.utility.pano.initialize_panorama(pano_canvas_id);
-
-              GEvent.addListener(map,"click", function(overlay, latlng) {
-                pano.setLocationAndPOV(latlng);
-              });
-            }
+          return map;
+        },
+        
+        add_overlays : function(map, overlays){
+          for(var i = 0; i < overlays.length; i++){
+            map.addOverlay(overlays[i]);
           }
         },
-      
+        
         save_lat_lng_data_point : function(latlng){
           $.post($(this).data("app_info").new_step_path, {"step[recipe_id]":$(this).data("app_info").recipe_id}, null, "script");
           return false;
@@ -46,16 +34,18 @@ var brio = (function(){
       },
 
       pano: {
-        initialize_panorama : function(canvas_id){
-          return new GStreetviewPanorama(document.getElementById(canvas_id));
+        initialize_pano : function(map, pano, gEvent){
+          gEvent.addListener(map, "click", function(overlay, latlng) {
+            pano.setLocationAndPOV(latlng);
+          });
+          
+          return pano;
         },
-
-        animate : function(lat, lng, canvas_id){
-          var myPano = new GStreetviewPanorama(document.getElementById(canvas_id));
-          var location = new GLatLng(lat, lng);
-          myPano.setLocationAndPOV(location, myPano.getPOV());
+        
+        animate : function(lat_lng, pano){
+          pano.setLocationAndPOV(lat_lng, pano.getPOV());
         }
       }
     }
   }
-})();
+})(jQuery);
--- test/javascript/brio_spec.js
+++ test/javascript/brio_spec.js
@@ -3,26 +3,98 @@ require("../../public/javascripts/brio.js");
 
 Screw.Unit(function() {
   describe("brio", function() {
+    
     after(function(){ teardownFixtures() });
     
     describe("utility", function(){
-
-      describe("register_date_picker", function(){
-        it("takes a list of div ids and runs the datepicker jQuery ui method on them", function(){
-          fixture($('<div id="brittonstest" />'));
-
-          var test_datepicker = $('#brittonstest');
-          date_picker_mock = mock(test_datepicker);
-          date_picker_mock.should_receive('datepicker').exactly('once');
+      
+       describe("contains utility methods for the brio applications", function(){
         
-          brio.utility.register_date_picker([test_datepicker]);
+        describe("register_date_picker", function(){
+          it("takes a list of div ids and runs the datepicker jQuery ui method on them", function(){
+            fixture($('<div id="brittonstest" />'));
+
+            var test_datepicker = $('#brittonstest');
+            date_picker_mock = mock(test_datepicker);
+            date_picker_mock.should_receive('datepicker').exactly('once');
+        
+            brio.utility.register_date_picker([test_datepicker]);
+          });
+        });
+       
+        describe("set_data", function(){
+          it("uses the jQuery data method to store data with an element in the dom", function(){
+            fixture($('<div id="brittontest" />'));
+            
+            brio.utility.set_data($('#brittontest'), [["mytest", 123]]);
+            expect($('#brittontest').data("mytest")).to(equal, 123);
+          });
         });
         
         describe("map", function(){
+          var googleMapMock;
+          var googleLatLngMock;
+          var googleOverlay;
+          
+          before(function(){
+            googleMapMock = Smoke.Mock();
+            googleLatLngMock = Smoke.Mock();
+            googleOverlay = Smoke.Mock();
+          });
+          
+          describe("contains utility methods pertaining to google maps", function(){
+            describe("initialize_map", function(){
+              it("takes a lat, lng, map and pano, and an array of overlays and creates a google map", function(){
+                googleMapMock = mock(new Object);
+                googleLatLngMock = mock(new Object);
+                googleMapMock.should_receive('setCenter').with_arguments(googleLatLngMock, 15).exactly('once');
+                googleMapMock.should_receive('setUIToDefault').exactly('once');
+                
+                brio.utility.map.initialize_map(googleMapMock, googleLatLngMock, 15);
+              });
+            });
+            
+            describe("add_overlays", function(){
+              it("adds google maps overlays passed into the method in an array to a google map", function(){
+                googleMapMock = mock(new Object);
+                googleMapMock.should_receive('addOverlay').with_arguments(googleOverlay).exactly('twice');
 
-          describe("initialize_map", function(){
-            it("takes a lat, lng, div id for the map and pano, and an array of overlays and creates a google map", function(){
+                brio.utility.map.add_overlays(googleMapMock, [googleOverlay, googleOverlay]);
+              });
+            });
+          });
+        });
+        
+        describe("pano", function(){
+          var googleMapMock;
+          var googleLatLngMock;
+          var googlePanoMock;
+          var googleEventMock;
+          
+          before(function(){
+            googleMapMock = Smoke.Mock();
+            googleLatLngMock = Smoke.Mock();
+            googlePanoMock = Smoke.Mock();
+            googleEventMock = Smoke.Mock();
+          });
 
+          describe("contains untility methods pertaining to google panos", function(){
+
+            describe("initialize_pano", function(){
+              it("initializes a new google maps pano object", function(){
+                googleEventMock.should_receive('addListener').exactly('once');
+                
+                brio.utility.pano.initialize_pano(googleMapMock, googlePanoMock, googleEventMock);
+              });
+            });
+            
+            describe("animate", function(){
+              it("should move a pano from one lat lng to another", function(){
+                googlePanoMock.should_receive('getPOV').exactly('once').and_return('looking south');
+                googlePanoMock.should_receive('setLocationAndPOV').with_arguments(googleLatLngMock, 'looking south').exactly('once');
+                
+                brio.utility.pano.animate(googleLatLngMock, googlePanoMock);
+              });
             });
           });
         });
@@ -30,4 +102,3 @@ Screw.Unit(function() {
     });
   });
 });
-
