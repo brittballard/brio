944c06fd696ea743b0a86b29a1412073f0bd12cb
Geoffrey <foeggy@gschiff.(none)>
2010-04-14 18:49:14 -0500

    hacked up signup logic

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -8,7 +8,10 @@ class SignupsController < ApplicationController
     @signup.event = Event.find(@signup.event_id)
     
     @signup.user = @signup.user_id.present? ? User.find(@signup.user_id) : User.new(params[:user])
-    
+    @signup.user.user_state = "pending"
+    @signup.user.email = ""
+    @signup.user.save(false)
+      
     cookies[@signup.event_id] = @signup.accepted_terms
     
     if @signup.save
@@ -22,28 +25,36 @@ class SignupsController < ApplicationController
   def register
     @signup = Signup.find(params[:id])
       
-    if cookies[@signup.event_id].nil? or cookies[@signup.event_id].empty? or cookies[@signup.event_id] = false
+    if cookies[@signup.event_id].nil? or cookies[@signup.event_id].empty? or cookies[@signup.event_id] == false
       redirect_to events_path
       flash[:notice] = "You didn't accept the terms."
     else
       #need to verify this is a valid signup for the current user
       #also need to hook up being able to sign in from here, and populate data from the signed in user, if possible
-      @user = User.new()
+      @user = @signup.user
     end
   end
   
   def go_pay
-    @user = User.new(params[:user])
+    #@user = User.new(params[:user])
     @signup = Signup.find(params[:id])
-
-    if @user.save
+    
+    if @current_user.nil? and params[:show_password]["show_password"] == "0"
+      debugger
+      @user.save(false)
+      @signup.user_id = @user.id
+      @signup.user = @user
+      if @signup.save
+        render(:action => 'pay')
+      end
+    elsif @user.save
       @signup.user_id = @user.id
       
       if @signup.save
         render(:action => 'pay')
       end
     else
-      flash[:error] = "Error saving!"
+      render 'register'
     end
   end
   
